a5c98d97b95f97f3a0925b307a9b950a39f9785a
hsw0905 <hsw0905@gmail.com>
2022-11-06 22:17:32 +0900

    init

---
+++ app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const ajax = new XMLHttpRequest();
+
+ajax.open('GET','https://api.hnpwa.com/v0/news/1.json', false);
+ajax.send();
+
+const newsFeed = JSON.parse(ajax.response);
+
+document.getElementById('root').innerHTML = `<ul>
+  <li>${newsFeed[0].title}</li>
+  <li>${newsFeed[1].title}</li>
+  <li>${newsFeed[2].title}</li>
+</ul>`;
