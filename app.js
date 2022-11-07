1e8dcb4ec6e3d1c2df4800958997159eaf9afce8
hsw0905 <hsw0905@gmail.com>
2022-11-07 10:29:41 +0900

    minimal

--- app.js
+++ app.js
@@ -2,13 +2,17 @@
 
 const ajax = new XMLHttpRequest();
 
-ajax.open('GET','https://api.hnpwa.com/v0/news/1.json', false);
+
+ajax.open('GET', 'https://api.hnpwa.com/v0/news/1.json', false);
 ajax.send();
 
 const newsFeed = JSON.parse(ajax.response);
+const ul = document.createElement('ul');
 
-document.getElementById('root').innerHTML = `<ul>
-  <li>${newsFeed[0].title}</li>
-  <li>${newsFeed[1].title}</li>
-  <li>${newsFeed[2].title}</li>
-</ul>`;
+for (let i = 0; i < 10; i++) {
+  const li = document.createElement('li');
+  li.innerHTML = newsFeed[i].title;
+  ul.appendChild(li);
+}
+
+document.getElementById('root').appendChild(ul);
